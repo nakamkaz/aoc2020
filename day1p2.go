16b21acae57106bb8e810bd39e92d49871c26cb7
Kazuo Nakamura <nakamkaz@kodain.com>
2020-12-03 18:02:19 +0900

    Create day1p2.go

---
+++ day1p2.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+        "bufio"
+        "fmt"
+        "log"
+        "os"
+        "strconv"
+)
+
+func StrsliceFromStdin() []string {
+        var dat []string = nil
+        nscan := bufio.NewScanner(os.Stdin)
+        for nscan.Scan() {
+                dat = append(dat, nscan.Text())
+        }
+        return dat
+}
+
+func ToIntsliceFromStrslice(ss []string) []int {
+        var dat []int = nil
+        for _, str := range ss {
+                v, err := strconv.Atoi(str)
+                dat = append(dat, v)
+                if err != nil {
+                        log.Fatal(err)
+                }
+        }
+        return dat
+}
+func main() {
+        ssitem := StrsliceFromStdin()
+        isitem := ToIntsliceFromStrslice(ssitem)
+        for _, i := range isitem {
+                for _, j := range isitem {
+                        for _, k := range isitem {
+                                if i+j+k == 2020 {
+                                        fmt.Println(i * j * k)
+                                        break
+                                }
+                        }
+                }
+        }
+}
